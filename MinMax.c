31d452fe6ce4848a77bb8ade90f5db4f0e104587
Park-Jongil <always97@nate.com>
2025-01-20 17:47:51 +0900

    2025.01.20 MinMax Append

---
+++ MinMax.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int		i,iMin,iMax;
+	int		iArray[16];
+	
+	printf("Input 10 Numbers = ");
+	for(i=0;i<10;i++) scanf("%d",&iArray[i]);	
+	iMin = iMax = iArray[0];
+	for(i=0;i<10;i++) {
+// 최대랑 최소숫자 찾기 	
+	}
+	printf("Output 10 Numbers = ");
+	for(i=0;i<10;i++) printf("%d ",iArray[i]);	
+	printf("\nMin Number = %d\n",iMin);
+	printf("Max Number = %d\n",iMax);
+
+	return(0);
+}
+
