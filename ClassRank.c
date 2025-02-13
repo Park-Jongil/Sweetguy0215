2757589ed752213f5744ff5d1d9350662348a059
Jongil_Park <always97@nate.com>
2025-02-13 09:26:36 +0900

    2025.02.13 성적처리 코드추가

--- ClassRank.c
+++ ClassRank.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	int		iKorean[10],iEnglish[10],iMath[10],iRank[10],i;
+	int		iKorean[10],iEnglish[10],iMath[10],iRank[10],iSum[10],i,j;
 	
 	srand(time(NULL));   
 	for(i=0;i<10;i++) {
@@ -13,9 +13,18 @@ int main(void)
         iMath[i] = rand() % 100;
         iRank[i] = 1;
     }
-	printf("Korean\tEnglish\tMath\tRank\n");
 	for(i=0;i<10;i++) {
-        printf("%d\t%d\t%d\t%d\n",iKorean[i],iEnglish[i],iMath[i],iRank[i]);
+        iSum[i] = iKorean[i] + iEnglish[i] + iMath[i];
+    }
+	for(i=0;i<10;i++) {
+        for(j=0;j<10;j++) {
+            if (iSum[i]<iSum[j]) iRank[i]++;
+        }
+    }
+
+    printf("Korean\tEnglish\tMath\tSum\tRank\n");
+	for(i=0;i<10;i++) {
+        printf("%d\t%d\t%d\t%d\t%d\n",iKorean[i],iEnglish[i],iMath[i],iSum[i],iRank[i]);
     }
   	return 0;
 }
